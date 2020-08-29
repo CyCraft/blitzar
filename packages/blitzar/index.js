f26239d14f72e19b6d13e955d6d8a1b41d323018
Luca Ban <mesqueeb@gmail.com>
2020-08-29 11:27:42 +0900

    build: prepare for release

---
+++ packages/blitzar/index.js
@@ -0,0 +1,12 @@
+import Form from '@blitzar/form'
+import Table from '@blitzar/table'
+
+export * from '@blitzar/form'
+export * from '@blitzar/table'
+
+export default {
+  install(Vue) {
+    Vue.use(Form)
+    Vue.use(Table)
+  },
+}
