9a80f3c64f2b1310ab697edab5367265f9479940
Luca Ban <mesqueeb@gmail.com>
2020-10-08 14:17:12 +0900

    chore: update dependencies

--- packages/docs/src/components/AnchorLink.vue
+++ packages/docs/src/components/AnchorLink.vue
@@ -54,7 +54,7 @@ export default defineComponent({
   name: 'AnchorLink',
   props: {
     content: { type: String, required: true },
-    href: { type: String, required: true },
+    href: { type: String },
     external: { type: Boolean, default: false },
   },
 })
