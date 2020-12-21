39251559e4ec2f96bdef73e910cc01ff495a87d9
Luca Ban <mesqueeb@gmail.com>
2020-12-21 14:29:22 +0900

    chore: delete unused dev project

--- packages/dev/src/components/atoms/form/helpers/focusIfInputEl.js
+++
@@ -1,5 +0,0 @@
-export default function focusIfInputEl(e) {
-  if (!e || !e.srcElement) return
-  if (e.srcElement.nodeName === 'INPUT') e.srcElement.focus()
-  if (e.srcElement.nodeName === 'TEXTAREA') e.srcElement.focus()
-}
