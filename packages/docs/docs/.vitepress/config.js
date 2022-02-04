348421fc350c5072bde2f355d549c51a3c09abb9
Luca Ban <mesqueeb@users.noreply.github.com>
2022-02-04 17:22:07 +0900

    feat: greatly improve Blitzar docs

--- packages/docs/docs/.vitepress/config.js
+++ packages/docs/docs/.vitepress/config.js
@@ -1,8 +1,39 @@
 module.exports = {
+  lang: 'en-US',
   title: 'Blitzar Docs',
   description: 'Blitzar Vue 3 Form & Table Generator Documentation',
   markdown: {
     // options for markdown-it-toc
     toc: { includeLevel: [1, 2] },
+  },
+  themeConfig: {
+    logo: '/blitzar-logo-white.svg',
+    repo: 'cycraft/blitzar',
+
+    docsDir: 'packages/docs/docs',
+    docsBranch: 'production',
+    editLinks: true,
+    editLinkText: 'Edit this page on GitHub',
+    lastUpdated: 'Last Updated',
+
+    nav: [
+      {
+        text: 'Components',
+        items: [
+          { text: 'BlitzForm', link: '/blitz-form/', activeMatch: '^/$|^/blitz-form/' },
+          { text: 'BlitzTable', link: '/blitz-table/', activeMatch: '^/$|^/blitz-table/' },
+          {
+            text: 'BlitzListForm',
+            link: '/blitz-list-form/',
+            activeMatch: '^/$|^/blitz-list-form/',
+          },
+          { text: 'Advanced', link: '/advanced/', activeMatch: '^/$|^/advanced/' },
+        ],
+      },
+      {
+        text: 'Release Notes',
+        link: 'https://github.com/cycraft/blitzar/releases',
+      },
+    ],
   },
 }
