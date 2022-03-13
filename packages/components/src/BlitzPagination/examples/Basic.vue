4fbea17660840372c0447ee16582a461f3b91a53
Luca Ban <mesqueeb@users.noreply.github.com>
2022-03-13 13:32:01 +0900

    add usage example BlitzPagination

---
+++ packages/components/src/BlitzPagination/examples/Basic.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+import BlitzPagination from '../BlitzPagination.vue'
+
+const pageNrA = ref(1)
+const pageNrB = ref(1)
+const pageNrC = ref(1)
+</script>
+
+<template>
+  pageNrA: <code>{{ pageNrA }}</code>
+  <BlitzPagination v-model="pageNrA" :pageCount="3" />
+
+  pageNrB: <code>{{ pageNrB }}</code>
+  <BlitzPagination v-model="pageNrB" :pageCount="9" />
+
+  pageNrC: <code>{{ pageNrC }}</code>
+  <BlitzPagination v-model="pageNrC" :pageCount="100" />
+</template>
